f0606632128927302c2c9a27a05bd5f051d8315f
kwamekyeimonies <kwamekyeimonies@gmail.com>
2025-01-18 23:39:01 +0000

    staging & backend engine setup

---
+++ src/main/java/com/example/logistics_management_engine/LogisticsManagementEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.logistics_management_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsManagementEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsManagementEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/logistics_management_engine/LogisticsManagementEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logistics_management_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsManagementEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
